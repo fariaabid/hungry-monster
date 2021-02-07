e651c989ab1990d7edd98384cc5eb617a21480e4
faria abid <faria.ju.2k16@gmail.com>
2021-02-07 21:51:05 +0600

    almost done

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ searchButton.addEventListener('click', function() {
         .then(res => res.json())
         .then(data => {
             let foodItemsInnerHtml = document.getElementById('allFoodItems').innerHTML = "";
-            let foodIngredientsDetail = document.getElementById('foodIngredientsDetail').innerHTML = ' ';
+            document.getElementById('foodIngredientsDetail').innerHTML = ' ';
             const allFoodItems = document.getElementById('allFoodItems');
             data.meals.forEach(fooditem => {
                 const foodBox = document.createElement('div')
                 foodBox.innerHTML = `
             <img src="${ fooditem.strMealThumb }" onClick="foodDetailsShow(${ fooditem.idMeal })">
-            <h1 onClick="foodDetailsShow(${ fooditem.idMeal })" >${ fooditem.strMeal }</h1>
+            <h3 onClick="foodDetailsShow(${ fooditem.idMeal })" >${ fooditem.strMeal }</h3>
             `;
                 foodBox.className = "card";
                 allFoodItems.appendChild(foodBox);
@@ -28,15 +28,15 @@ let foodDetailsShow = searchBoxInput => {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${ searchBoxInput }`)
         .then(res => res.json())
         .then(data => {
-            let foodData = document.getElementById('foodIngredientsDetail');
+            let foodItemData = document.getElementById('foodIngredientsDetail');
             document.getElementById('foodIngredientsDetail').innerHTML = ' ';
             document.getElementById('foodIngredientsDetail').style.display = 'block';
-            let foodDetails = document.createElement('div')
-            foodDetails.innerHTML = `
+            let foodIngredientsDetail = document.createElement('div')
+            foodIngredientsDetail.innerHTML = `
             <img src="${ data.meals[0].strMealThumb }">
             <h1>${ data.meals[0].strMeal }</h1>
             <br>
-            <h1>Ingredients:</h1>
+            <h4>Ingredients:</h4>
             <p><span class="ingredient">.</span>${data.meals[0].strIngredient1}</p>
             <p><span class="ingredient">.</span>${data.meals[0].strIngredient2}</p>
             <p><span class="ingredient">.</span>${data.meals[0].strIngredient3}</p>
@@ -44,7 +44,7 @@ let foodDetailsShow = searchBoxInput => {
             <p><span class="ingredient">.</span>${data.meals[0].strIngredient5}</p>
             <p><span class="ingredient">.</span>${data.meals[0].strIngredient6}</p>
             `;
-            foodDetails.className = "food-details";
-            foodData.appendChild(foodDetails);
+            foodIngredientsDetail.className = "food-items-details";
+            foodItemData.appendChild(foodIngredientsDetail);
         })
 }
