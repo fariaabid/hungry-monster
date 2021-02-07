fc01dc47bbda03363d3af8f09f0770a92b72b536
faria abid <faria.ju.2k16@gmail.com>
2021-02-07 20:50:34 +0600

    50% done

---
+++ script.js
@@ -0,0 +1,50 @@
+const searchButton = document.getElementById('searchButton');
+searchButton.addEventListener('click', function() {
+    const foodName = document.getElementById('foodInput').value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${ foodName }`)
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById('foods').innerHTML = "";
+            document.getElementById('foodsData').innerHTML = ' ';
+            const foods = document.getElementById('foods');
+            data.meals.forEach(element => {
+                const food = document.createElement('div')
+                food.innerHTML = `
+            <img src="${ element.strMealThumb }" onClick="foodDetailsShow(${ element.idMeal })">
+            <h1 onClick="foodDetailsShow(${ element.idMeal })" >${ element.strMeal }</h1>
+            `;
+                food.className = "card";
+                foods.appendChild(food);
+            });
+        })
+        .catch(error => {
+            const errorMessage = document.createElement('h3')
+            errorMessage.innerHTML = `Please input a valid food name.`;
+            foods.appendChild(errorMessage);
+        })
+})
+
+let foodDetailsShow = foodName => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${ foodName }`)
+        .then(res => res.json())
+        .then(data => {
+            let foodData = document.getElementById('foodsData');
+            document.getElementById('foodsData').innerHTML = ' ';
+            document.getElementById('foodsData').style.display = 'block';
+            let foodDetails = document.createElement('div')
+            foodDetails.innerHTML = `
+            <img src="${ data.meals[0].strMealThumb }">
+            <h1>${ data.meals[0].strMeal }</h1>
+            <br>
+            <h1>Ingredients:</h1>
+            <p><span class="ingredient">.</span>${data.meals[0].strIngredient1}</p>
+            <p><span class="ingredient">.</span>${data.meals[0].strIngredient2}</p>
+            <p><span class="ingredient">.</span>${data.meals[0].strIngredient3}</p>
+            <p><span class="ingredient">.</span>${data.meals[0].strIngredient4}</p>
+            <p><span class="ingredient">.</span>${data.meals[0].strIngredient5}</p>
+            <p><span class="ingredient">.</span>${data.meals[0].strIngredient6}</p>
+            `;
+            foodDetails.className = "food-details";
+            foodData.appendChild(foodDetails);
+        })
+}
